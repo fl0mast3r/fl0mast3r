b91eb40832bfe3f1b048a2eb9425f78656a458cc
fl0mDev <70860181+fl0mast3r@users.noreply.github.com>
2025-07-20 15:40:12 +0300

    Add files via upload

---
+++ build/assets/js/9c1e0f9d.6d6130f1.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[720],{4410:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/fl0mast3r/the-shopping-spree/blob/master","baseUrl":"/the-shopping-spree/","classOrder":[{"section":"Services","classes":["GenerationService"]},{"section":"Controllers","classes":["AnimationsController"]}],"apiCategories":[]}')}}]);
